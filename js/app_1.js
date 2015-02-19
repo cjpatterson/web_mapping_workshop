a96cc6604727b4fa58635da2c689a734f1a11745
cjpatterson <cjpatterson@users.noreply.github.com>
2015-02-19 15:54:45 -0600

    Update app_1.js

--- js/app_1.js
+++ js/app_1.js
@@ -26,7 +26,20 @@ featureLayer.on('ready', function(){
   });
   map.fitBounds(featureLayer.getBounds());
 });
+//Add Data 2
+var dataFileToAdd2 = 'homeroute.geojson';
 
+var featureLayer2 = L.mapbox.featureLayer();
+
+    featureLayer2.loadURL(dataFileToAdd2);
+    featureLayer2.addTo(map);
+
+featureLayer2.on('ready', function(){
+  this.setStyle({
+    "color": "#a90a0a"
+  });
+  //map.fitBounds(featureLayer.getBounds());
+});
 ///////////////////
 //Add popup
 
